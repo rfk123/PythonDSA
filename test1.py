b5616097b12366a39866aab52e24995fa204beb1
rfk123 <rfkamali@gmail.com>
2026-07-08 16:57:18 -0700

    Successfuly designed and tested a contains duplicate program in O(n) time complexity and O(n) space complexity

--- test1.py
+++ test1.py
@@ -1,6 +1,29 @@
 
-def contains_duplicate(nums):
-    pass
+def contains_duplicate(nums) -> bool:
+    # This function will check to see if an integer list contains duplicates
+    # Input -> a list of integers (positives and negatives)
+    # Output -> boolean (true or false)
+    """
+    First idea is that we can solve this in just one pass through of the nums array.
+    We create a set that stores unique integers found in nums.
+    Every iteration of nums we check to see if the integer is in the set. If it is then we have found our duplicate. If not, then we just store the current num in the set and move to the next.
+    If we get to the end of nums without finding a duplicate we can just return false.
+    """
+    if not nums:
+        return False
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+
+""" Test for contains_duplicate """
+# [] -> False
+# [1,2,3,4,-1,-2,-3,-4,0] -> False
+# [1,2,3,4,-1,2] -> True
+# [-2,3,4,-1,-2,10] -> True
 
 
 def two_sum(nums, target):
