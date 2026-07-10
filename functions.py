2be4627ddbe4b688486ade2c49a22d346c4315c5
rfk123 <rfkamali@gmail.com>
2026-07-10 08:28:23 -0700

    deep dive on functions in python

---
+++ functions.py
@@ -0,0 +1,11 @@
+
+# Functions are just reusable blocks of code that performs specific tasks
+location = "Baltimore"
+
+
+def welcome_message(name: str, greeting="Hello") -> None:
+    location = "Maryland"
+    print(f"{greeting}, {name} from {location}!")
+
+
+welcome_message("reza", "poopy")
