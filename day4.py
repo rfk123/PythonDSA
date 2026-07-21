f58fceb8fff3af67c0031f084b9dd8686b5a784c
rfk123 <rfkamali@gmail.com>
2026-07-20 20:28:36 -0700

    Successfully solved and passed all test cases for leetcode group_anagrams.

--- day4.py
+++ day4.py
@@ -30,3 +30,29 @@ def word_pattern(pattern: str, sentence: str) -> bool:
             return False
 
     return True
+
+
+def group_anagrams(words: list[str]) -> list[list[str]]:
+    anagrams = {}
+    for word in words:
+        char_frequency = [0] * 26
+        for char in word:
+            index = ord(char) - ord('a')
+            char_frequency[index] += 1
+        keys = tuple(char_frequency)
+        if keys not in anagrams:
+            anagrams[keys] = []
+        anagrams[keys].append(word)
+
+    return list(anagrams.values())
+
+
+"""
+So what did I learn/practice:
+
+I remembered that in python you can get the ascii value of a value with the ord() function (ord('a') is 97).
+I remembered that to insert a value into the end of a list in python you use .append.
+I remembered that in python keys in dictionaries must be hashable and therefore imutable data types such as strings or tuples.
+I learned that you can use the list() function to create a list of some data. Here I use list() to create a list storing each value in the hashmap.
+I remembered that you create a list of length n with [0] * n.
+"""
