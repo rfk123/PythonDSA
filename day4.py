4275063aa07be52b3da27a3bb9a7ed24abb2ccca
rfk123 <rfkamali@gmail.com>
2026-07-20 20:04:56 -0700

    Successfully solved word pattern in O(n) space and O(n) time complexity.

--- day4.py
+++ day4.py
@@ -13,3 +13,20 @@ def is_isomorphic(s: str, t: str) -> bool:
         if (t_count[t[i]] != s_count[s[i]]):
             return False
     return True
+
+
+def word_pattern(pattern: str, sentence: str) -> bool:
+    words = sentence.split(" ")
+    if len(words) != len(pattern):
+        return False
+
+    word_to_char = {}
+    char_to_word = {}
+
+    for char, word in zip(pattern, words):
+        if word in word_to_char and word_to_char[word] != char:
+            return False
+        if char in char_to_word and char_to_word[char] != word:
+            return False
+
+    return True
