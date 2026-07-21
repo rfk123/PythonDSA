cc6a6beee2aedf94b65cd3d55c832d512a5dd11e
rfk123 <rfkamali@gmail.com>
2026-07-20 20:45:36 -0700

    Note taking on algorithms

--- day4.py
+++ day4.py
@@ -15,6 +15,16 @@ def is_isomorphic(s: str, t: str) -> bool:
     return True
 
 
+"""
+The information stored in s_count is essentially just a character stored with its frequency count. Same for t_count.
+My thought process is that if we itterate through both strings and the current character's frequencies in both s and t differ then we have a mismatch and therefore the strings structures don't map to each other.
+The implementation essentially just ensures that the two strings are of the same length, create two hashmaps to store frequency counts for each word, loop through each word and check frequencies to see if there is an early exit, and then once loop done return true.
+The time complexity is O(n) because at the worst case we visit each character in both strings but in just one pass so since they are the same size it is O(n).
+The space complexity is O(n) because if each character in the strings were unique then the hashmap lengths would match the length of the strings. however there are only 26 possible strings to consider.
+test cases could be strings of different sizes, strings like 'aappl' and 'bbllp' (would return true), and 'aaaaa' with 'bbbbb' (would return true)
+"""
+
+
 def word_pattern(pattern: str, sentence: str) -> bool:
     words = sentence.split(" ")
     if len(words) != len(pattern):
@@ -55,4 +65,5 @@ I remembered that to insert a value into the end of a list in python you use .ap
 I remembered that in python keys in dictionaries must be hashable and therefore imutable data types such as strings or tuples.
 I learned that you can use the list() function to create a list of some data. Here I use list() to create a list storing each value in the hashmap.
 I remembered that you create a list of length n with [0] * n.
+I learned that you can use zip(x,y) to loop over both x and y at the same time
 """
