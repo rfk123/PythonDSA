86d7bfe4c0f6244d8b941b3aa16eb0657dcc409a
rfk123 <rfkamali@gmail.com>
2026-07-23 18:58:28 -0700

    Solved a variable-szie sliding window pattern problem: longest substring without repeating

---
+++ day7.py
@@ -0,0 +1,31 @@
+
+# Time to move forward in my sliding window pattern practice
+# Time to practice variable-sized sliding window pattern
+# Variable-sized window is just a window that's size shrinks and grows depending on some condition
+
+# Lets first start out with the problem: longest substring without repeating characters
+def length_of_longest_substrings(s: str) -> int:
+    # What do I need to store? I need to keep track of what value I have seen so far in my set (the values that the window contain). All of these should be unique
+    # Whats something to consider? Since we dont need to store any information other than what values coinside in the window, we can use a set.
+    # If the read pointer goes out into the string and finds a character that already exists in the window then we shrink the window from the left until that is no longer the case
+    # At every valid window we compare the current window's length with the max length
+    if len(s) <= 0:
+        return 0
+    window_chars = set()
+    left = 0
+    right = 0
+    max_length = 0
+    while right < len(s):
+        while s[right] in window_chars:
+            window_chars.discard(s[left])
+            left += 1
+        window_chars.add(s[right])
+        max_length = max(max_length, right - left + 1)
+        right += 1
+    return max_length
+
+
+print(length_of_longest_substrings("abcabcbb"))
+print(length_of_longest_substrings("bbbbbb"))
+print(length_of_longest_substrings("abba"))
+print(length_of_longest_substrings(""))
