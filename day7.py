ecfc4d2cc441e86155bd3e98ab16c28b6bab3c44
rfk123 <rfkamali@gmail.com>
2026-07-23 19:12:02 -0700

    Able to use variable-sized window to shrink a window while condition is met in order to get the minimum length substring

--- day7.py
+++ day7.py
@@ -29,3 +29,22 @@ print(length_of_longest_substrings("abcabcbb"))
 print(length_of_longest_substrings("bbbbbb"))
 print(length_of_longest_substrings("abba"))
 print(length_of_longest_substrings(""))
+
+
+def min_subarray_len(target: int, nums: list[int]) -> int:
+
+    current_sum = 0
+    min_length = float("inf")
+    left = 0
+    right = 0
+    while (right < len(nums)):
+        current_sum += nums[right]
+        while current_sum >= target:
+            min_length = min(min_length, right - left + 1)
+            current_sum -= nums[left]
+            left += 1
+        right += 1
+    return 0 if min_length == float("inf") else min_length
+
+
+print(min_subarray_len(7, [2, 3, 1, 2, 4, 3]))
