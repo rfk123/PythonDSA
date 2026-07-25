dd1e3b66f2c1354c547b4a4b3ce2e0e30bde852a
rfk123 <rfkamali@gmail.com>
2026-07-24 22:15:00 -0700

    solved longest subarray with atmost k zeroes in O(n) time and O(1) space.

--- day8.py
+++ day8.py
@@ -33,3 +33,45 @@ def contains_duplicate_within_k(nums: list[int], k: int) -> bool:
 print(contains_duplicate_within_k([1, 2, 3, 1], 3))
 print(contains_duplicate_within_k([1, 2, 3, 1], 2))
 print(contains_duplicate_within_k([1, 0, 1, 1], 1))
+
+
+def longest_subarray_at_most_k_zeroes(nums: list[int], k: int) -> int:
+    zeroes_count = 0
+    left = 0
+    right = 0
+    max_length = 0
+    while right < len(nums):
+        if nums[right] == 0:
+            zeroes_count += 1
+        while zeroes_count > k:
+            if nums[left] == 0:
+                zeroes_count -= 1
+            left += 1
+        max_length = max(max_length, right - left + 1)
+        right += 1
+    return max_length
+
+
+print(longest_subarray_at_most_k_zeroes(
+    [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2))  # 6
+
+print(longest_subarray_at_most_k_zeroes([0, 0, 1, 1, 0], 1))  # 3)
+
+
+def two_sum_sorted(nums: list[int], target: int) -> list[int]:
+    left = 0
+    right = len(nums)
+    while left < right:
+        sum = nums[right] + nums[left]
+        if sum == target:
+            return [left, right]
+        elif sum > target:
+            right -= 1
+        else:
+            left += 1
+    return []
+
+
+print(two_sum_sorted([2, 7, 11, 15], 9))  # [0, 1]
+print(two_sum_sorted([1, 2, 3, 4, 6], 6))  # [1, 3]
+print(two_sum_sorted([1, 2, 3], 10))      # []
