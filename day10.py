0fe8d70f4298e2fc9ef0510449da219e2f4c6114
rfk123 <rfkamali@gmail.com>
2026-07-28 22:15:21 -0700

    Solved the isHappy leetcode problem with minor bugs.

--- day10.py
+++ day10.py
@@ -1,12 +1,23 @@
 
 def isHappy(n: int) -> bool:
     # a number is happy if you continuously add the squares of n until they sum to 1
+    seen = set()
+    while n != 1:
+        # we can detect a cycle if we revisit any previous version of n
+        if n in seen:
+            return False
+        seen.add(n)
+        n = helper(n)
+    return True
 
 
 def helper(n: int) -> int:
     total = 0
     while n > 0:
-        digit = n % 10
+        digit += n % 10
         total = digit ** 2
         n //= 10
     return total
+
+
+print(isHappy(19))
