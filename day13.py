efc23b640fa775df6e600e8cc3a44fa58c07abc4
rfk123 <rfkamali@gmail.com>
2026-08-05 19:52:45 -0700

    More DSA on longest contiguous subtring with at most 2 distinct characters.

--- day13.py
+++ day13.py
@@ -27,3 +27,18 @@ print(longest_two_distinct("eceba"))   # 3, from "ece"
 print(longest_two_distinct("ccaabbb"))  # 5, from "aabbb"
 print(longest_two_distinct(""))        # 0
 print(longest_two_distinct("aaaa"))    # 4
+
+
+"""
+| `right` | Incoming | Counts after adding | Shrinking performed | `left` after shrinking | Valid window | Maximum |
+| ------: | -------- | ------------------- | ------------------- | ---------------------: | ------------ | ------: |
+|       0 | `"e"`    |  {'e': 1}           | -                   | 0                      | 'e'          | 1       |
+|       1 | `"c"`    |  {'e':1, 'c':1}     | -                   | 0                      | 'ec'         | 2       |
+|       2 | `"e"`    |  {'e':2, 'c':1}     | -                   | 0                      | 'ece'        | 3       |
+|       3 | `"b"`    |  {'e':2, 'c':1, 'b':1}| removed e then c  | 2                      | 'eb'         | 3       |
+|       4 | `"a"`    |  {'e':1, 'b':1, 'a':1}| removed e         | 3                      | 'ba'         | 3       |
+
+Time complexity: O(n)
+Space Complexity: O(n)
+Why delete zero frequency key: We delete that entry in our dictionary when its value hits 0 because that means we no longer have that character in our window.
+"""
