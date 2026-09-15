30380206cc8bdf656333bc3f5792d742291169fb
rfk123 <rfkamali@gmail.com>
2026-09-15 16:25:58 -0700

    Solved valid parenthesis using a stack and a dictionary.

--- day28.py
+++ day28.py
@@ -7,10 +7,23 @@ def is_valid(s: str) -> bool:
     Return True if every opening bracket is closed
     by the correct type in the correct order.
     """
-    pass
+    symbols = {'(': ')', '[': ']', '{': '}'}
+
+    stack = []
+
+    for char in s:
+        if char in symbols:
+            stack.append(char)
+        else:
+            if not stack:
+                return False
+            popped = stack.pop()
+            if symbols[popped] != char:
+                return False
+    return True if not stack else False
 
 
-is_valid("()[]{}")   # True
-is_valid("([{}])")   # True
-is_valid("(]")       # False
-is_valid("([)]")     # False
+print(is_valid("()[]{}"))   # True
+print(is_valid("([{}])"))   # True
+print(is_valid("(]"))       # False
+print(is_valid("(["))     # False
