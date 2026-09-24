8eb59a21cb4faba56dc23d34bb75a00932a8d1ef
rfk123 <rfkamali@gmail.com>
2026-09-23 21:05:08 -0700

    More active recall. Using binary search to find the first occurence of a target value in an ordered integer array.

--- checkpoint2.py
+++ checkpoint2.py
@@ -15,6 +15,29 @@ def length_of_longest_substring(s: str) -> int:
     return max_len
 
 
-print(length_of_longest_substring("abcabcbb"))  # 3
-print(length_of_longest_substring("bbbbb"))     # 1
-print(length_of_longest_substring("pwwkew"))    # 3
+# print(length_of_longest_substring("abcabcbb"))  # 3
+# print(length_of_longest_substring("bbbbb"))     # 1
+# print(length_of_longest_substring("pwwkew"))    # 3
+
+
+def first_occurrence(nums: list[int], target: int) -> int:
+    """
+    nums is sorted.
+    Return the first index containing target,
+    or -1 if target does not exist.
+    """
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            right = mid
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return left if nums[left] == target else -1
+
+
+print(first_occurrence([1, 2, 2, 2, 4, 5], 0))  # 1)
